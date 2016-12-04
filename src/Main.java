94d2672df5696224717083653ca6e9423f7ed9a1
ThomasSchaal <thomas.schaal@utt.fr>
2016-12-04 14:45:30 +0100

    Commit message

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * @author daussy
  *
  */
-public class Blahblah {
+public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("->1<- Chiffrement symétrique VCES");
